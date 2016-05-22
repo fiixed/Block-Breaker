d54e31217b673e8962c8081b5fbee715fc47ecd0
Andrew Bell <fiixed@gmail.com>
2016-05-22 08:51:25 -0500

    Game ready for testing

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brick : MonoBehaviour {
+
+	public static int brickCount = 0;
+
+	public AudioClip crack;
+	public Sprite[] hitSprites;
+
+	private LevelManager levelManager;
+	private int timesHit;
+
+	private bool isBreakable;
+
+	// Use this for initialization
+	void Start () {
+		isBreakable = (this.tag == "Breakable");
+		if (isBreakable) {
+			brickCount++;
+		}
+		timesHit = 0;
+		levelManager = GameObject.FindObjectOfType<LevelManager>();
+
+
+	}
+
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		AudioSource.PlayClipAtPoint(crack, transform.position);
+		if (isBreakable) {
+			HandleHits ();
+		}
+	}
+
+	void HandleHits() {
+		timesHit++;
+		int maxHits = hitSprites.Length + 1;
+		if (timesHit >= maxHits) {
+			brickCount--;
+			levelManager.BrickDestroyed();
+			Destroy(gameObject);
+		} else {
+			LoadSprites();
+		}
+	}
+
+	void LoadSprites() {
+		int spriteIndex = timesHit -1;
+		if (hitSprites[spriteIndex]) {
+			this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+		}
+
+	}
+
+	// TODO Remove this method once we can actually win
+
+	void SimulteWin() {
+		levelManager.LoadNextLevel();
+	}
+
+}
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -5,12 +5,20 @@ using System.Collections;
 public class LevelManager : MonoBehaviour {
 
 	public void LoadLevel(string name) {
-		Debug.Log("Level load requested for: " + name);
 		SceneManager.LoadScene(name);
 	}
 	
 	public void QuitRequest() {
-		Debug.Log("I wanna quit");
 		Application.Quit();
+	}
+
+	public void LoadNextLevel() {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	public void BrickDestroyed() {
+		if (Brick.brickCount <= 0) {
+			LoadNextLevel();
+	}
 	}
 }
